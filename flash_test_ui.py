62cc1ce50cebc0177e016034bbad04e4f43d51b4
Samer Armaly <40248744+BasicWalker@users.noreply.github.com>
2020-01-26 16:16:55 -0500

    ui with pyqt5

---
+++ flash_test_ui.py
@@ -0,0 +1,157 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'mainwindow.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 480)
+        MainWindow.setAutoFillBackground(False)
+        MainWindow.setStyleSheet("")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setStyleSheet("")
+        self.centralwidget.setObjectName("centralwidget")
+        self.frontspeed = QtWidgets.QLCDNumber(self.centralwidget)
+        self.frontspeed.setGeometry(QtCore.QRect(120, 30, 291, 151))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(1)
+        sizePolicy.setHeightForWidth(self.frontspeed.sizePolicy().hasHeightForWidth())
+        self.frontspeed.setSizePolicy(sizePolicy)
+        palette = QtGui.QPalette()
+        brush = QtGui.QBrush(QtGui.QColor(255, 108, 17))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.WindowText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(255, 108, 17))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.Text, brush)
+        brush = QtGui.QBrush(QtGui.QColor(255, 108, 17))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.ButtonText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(255, 108, 17, 128))
+        brush.setStyle(QtCore.Qt.NoBrush)
+        palette.setBrush(QtGui.QPalette.Active, QtGui.QPalette.PlaceholderText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(255, 108, 17))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.WindowText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(255, 108, 17))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.Text, brush)
+        brush = QtGui.QBrush(QtGui.QColor(255, 108, 17))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.ButtonText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(255, 108, 17, 128))
+        brush.setStyle(QtCore.Qt.NoBrush)
+        palette.setBrush(QtGui.QPalette.Inactive, QtGui.QPalette.PlaceholderText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(255, 108, 17))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.WindowText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(255, 108, 17))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.Text, brush)
+        brush = QtGui.QBrush(QtGui.QColor(255, 108, 17))
+        brush.setStyle(QtCore.Qt.SolidPattern)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.ButtonText, brush)
+        brush = QtGui.QBrush(QtGui.QColor(255, 108, 17, 128))
+        brush.setStyle(QtCore.Qt.NoBrush)
+        palette.setBrush(QtGui.QPalette.Disabled, QtGui.QPalette.PlaceholderText, brush)
+        self.frontspeed.setPalette(palette)
+        self.frontspeed.setStyleSheet("QLCDNumber{\n"
+"    color: rgb(255,108,17) ;\n"
+"}")
+        self.frontspeed.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.frontspeed.setSegmentStyle(QtWidgets.QLCDNumber.Flat)
+        self.frontspeed.setProperty("intValue", 60)
+        self.frontspeed.setObjectName("frontspeed")
+        self.carspeed = QtWidgets.QLCDNumber(self.centralwidget)
+        self.carspeed.setGeometry(QtCore.QRect(390, 30, 291, 151))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(1)
+        sizePolicy.setHeightForWidth(self.carspeed.sizePolicy().hasHeightForWidth())
+        self.carspeed.setSizePolicy(sizePolicy)
+        self.carspeed.setStyleSheet("QLCDNumber{\n"
+"    color: rgb(255,108,17) ;\n"
+"}")
+        self.carspeed.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.carspeed.setSegmentStyle(QtWidgets.QLCDNumber.Flat)
+        self.carspeed.setProperty("intValue", 55)
+        self.carspeed.setObjectName("carspeed")
+        self.Background = QtWidgets.QLabel(self.centralwidget)
+        self.Background.setGeometry(QtCore.QRect(0, -30, 821, 511))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(1)
+        sizePolicy.setVerticalStretch(1)
+        sizePolicy.setHeightForWidth(self.Background.sizePolicy().hasHeightForWidth())
+        self.Background.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setPointSize(30)
+        self.Background.setFont(font)
+        self.Background.setStyleSheet("color: rgb(45, 226, 230);")
+        self.Background.setText("")
+        self.Background.setPixmap(QtGui.QPixmap(r'C:\Users\mysam\Desktop\FlashUI\FlashCarPi\assets\flashretrobackground.jpg'))
+        self.Background.setAlignment(QtCore.Qt.AlignCenter)
+        self.Background.setObjectName("Background")
+        self.rpm = QtWidgets.QLCDNumber(self.centralwidget)
+        self.rpm.setGeometry(QtCore.QRect(130, 190, 100, 78))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(2)
+        sizePolicy.setVerticalStretch(2)
+        sizePolicy.setHeightForWidth(self.rpm.sizePolicy().hasHeightForWidth())
+        self.rpm.setSizePolicy(sizePolicy)
+        self.rpm.setStyleSheet("QLCDNumber{\n"
+"    color: rgb(255,108,17) ;\n"
+"}")
+        self.rpm.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.rpm.setSegmentStyle(QtWidgets.QLCDNumber.Flat)
+        self.rpm.setProperty("intValue", 5)
+        self.rpm.setObjectName("rpm")
+        self.distance = QtWidgets.QLCDNumber(self.centralwidget)
+        self.distance.setGeometry(QtCore.QRect(560, 210, 111, 42))
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(3)
+        sizePolicy.setVerticalStretch(3)
+        sizePolicy.setHeightForWidth(self.distance.sizePolicy().hasHeightForWidth())
+        self.distance.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setPointSize(8)
+        self.distance.setFont(font)
+        self.distance.setStyleSheet("QLCDNumber{\n"
+"    color: rgb(255,108,17) ;\n"
+"}")
+        self.distance.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.distance.setSmallDecimalPoint(False)
+        self.distance.setSegmentStyle(QtWidgets.QLCDNumber.Flat)
+        self.distance.setProperty("value", 10.0)
+        self.distance.setObjectName("distance")
+        self.frontspeed.raise_()
+        self.Background.raise_()
+        self.rpm.raise_()
+        self.distance.raise_()
+        self.carspeed.raise_()
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
